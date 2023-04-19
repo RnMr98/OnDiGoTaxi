7faa10c4e45f9ac1e6855bc9d8cda95bbd215478
RnMr98 <125484084+RnMr98@users.noreply.github.com>
2023-04-19 15:16:41 -0500

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+    $("#form").validate();
+    });
+
+
+
+
+
+  const form = document.getElementsByName('form');
+  const name = document.getElementsByName('name');
+  const number = document.getElementsByName('number');
+  const pickup = document.getElementsByName('pickup');
+  const dropoff = document.getElementsByName('dropoff');
+  const passengers = document.getElementsByName('passengers');
+  
+  form.addEventListener('submit', e => {
+      e.preventDefault();
+  
+      validateInputs();
+  });
+    
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+    
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success')
+    }
+    
+    const setSuccess = element => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+    
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    };
+    
+   
+    const validateInputs = () => {
+      const nameValue = name.value.trim();
+      const numberValue = number.value.trim();
+      const pickupValue = pickup.value.trim();
+      const dropoffValue = dropoff.value.trim(); 
+      const passengersValue = passengers.value.trim();
+
+
+        if(nameValue === '') {
+            setError(name, 'Your name is required');
+        } else {
+            setSuccess(name);
+        }
+    
+        if(numberValue === '') {
+            setError(number, 'Phone Number is required');
+        }  else if (numberValue.length < 10 || numberValue.length > 11 ) {
+          setError(number, 'Enter a 10 digit Phone Number.');
+        } else {
+            setSuccess(number);
+        }
+    
+        if(pickupValue === '') {
+            setError(pickup, 'Pick up Location is required');
+        } else {
+            setSuccess(pickup);
+        }
+    
+        if(dropoffValue === '') {
+            setError(dropoff, 'Drop off Location is required');
+        } else {
+            setSuccess(dropoff);
+        }
+
+        if(passengersValue === '') {
+          setError(passengers, 'please state the number of passenger');
+        } else {
+            setSuccess(passengers);
+        }
+    
+    };
+
+
+    
