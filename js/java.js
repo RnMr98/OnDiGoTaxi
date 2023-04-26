bc61b6b7a9a9a67792f4edb6ee8aae4125fd2cea
RnMr98 <125484084+RnMr98@users.noreply.github.com>
2023-04-25 22:17:10 -0500

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,3 +1,18 @@
+
     function clickme(){
         alert("your message has been sent");
     }
+
+    // Get all input elements
+const inputs = document.querySelectorAll('input, select');
+
+// Add event listener to each input element
+inputs.forEach(input => {
+  input.addEventListener('blur', () => {
+    // Check if input has a value
+    if (input.value.trim() === '') {
+      // Show alert if input is empty
+      alert(`${input.name} is required`);
+    }
+  });
+});
